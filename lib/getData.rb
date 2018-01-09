b7ac5846ae94b5f7845624236ebd09cce2c85a12
rolandosorbelli <rolando.sorbelli@gmail.com>
2018-01-09 14:11:24 +0000

    Ran tests on getData.rb

--- lib/getData.rb
+++ lib/getData.rb
@@ -2,15 +2,15 @@ require "httparty"
 
 class GetData
 
-	def purchases_data
-    purchases = 'https://driftrock-dev-test-2.herokuapp.com/purchases?per_page=1000&page=1'
-    response = HTTParty.get(purchases)
-    response.parsed_response['data']
-  end
-
   def users_data
     users = 'https://driftrock-dev-test-2.herokuapp.com/users?per_page=1000&page=1'
     response = HTTParty.get(users)
+    response.parsed_response['data']
+  end
+
+	def purchases_data
+    purchases = 'https://driftrock-dev-test-2.herokuapp.com/purchases?per_page=1000&page=1'
+    response = HTTParty.get(purchases)
     response.parsed_response['data']
   end
 end
