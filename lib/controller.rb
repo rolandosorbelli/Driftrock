1a316a160ec347030c547ed6113d589e78a92f5d
rolandosorbelli <rolando.sorbelli@gmail.com>
2018-01-09 15:57:14 +0000

    Created method to calculate the total of a user

--- lib/controller.rb
+++ lib/controller.rb
@@ -5,14 +5,24 @@ class Controller
 
 	attr_reader :getData, :user
 
-	def initialize
-		@getData = GetData.new
-		@user = Users.new
+	def initialize(getData = GetData.new, user = Users.new)
+		@getData = getData
+		@user = user
 	end
 
 	def run(arg)
-		puts @getData.purchases_data
-		
+		if arg[0] == 'total_spend'
+      total_spend(arg[1])
+    else
+      puts 'The email has not been recognised'
+    end
 	end
+
+	def total_spend(email)
+    user_data = getData.users_data
+    purchase_data = getData.purchases_data
+    user_id = user.user_id(email, user_data)
+    puts user.calculate_total_spend(user_id, purchase_data)
+  end
 
 end
--- lib/users.rb
+++ lib/users.rb
@@ -3,17 +3,29 @@ require "./lib/getData.rb"
 class Users
 	attr_reader :getData
 
-	def initialize
-		@getData = GetData.new
+	def initialize(getData = GetData.new)
+		@getData = getData
 	end
 
 	def users_data
-		@getData.get_user_data
-	end
+     getData.users_data
+   	end
+
+   def purchases_data
+     getData.purchases_data
+   end
 
 	def user_id(email, users_data)
 		users_data.each do |user|
 			return user["id"] if user["email"] == email
 		end
+	end
+
+	def calculate_total_spend(user_id, purchases_data)
+		total = 0
+		purchases_data.each do |purchase|
+			total += purchase['spend'].to_f if purchase['user_id'] == user_id
+     	end
+    total
 	end
 end
