9c02eaccc6bb0cec22922e20dd42686306cc00ac
rolandosorbelli <rolando.sorbelli@gmail.com>
2018-01-09 13:15:45 +0000

    Modified controller.rb
    - Created a method that gets data from purchases endpoint
    - Created a method that gets data from users endpoint
    - Install httparty gem to be able to get data from the API

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,2 +1,14 @@
 class Controller
+
+	def purchases_data
+    purchases = 'https://driftrock-dev-test-2.herokuapp.com/purchases?per_page=1000&page=1'
+    response = HTTParty.get(purchases)
+    response.parsed_response['data']
+  end
+
+  def users_data
+    users = 'https://driftrock-dev-test-2.herokuapp.com/users?per_page=1000&page=1'
+    response = HTTParty.get(users)
+    response.parsed_response['data']
+  end
 end
