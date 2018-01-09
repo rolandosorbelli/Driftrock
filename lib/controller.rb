53523b5d0602299c066f9cafa05fbfbd7d7703ca
rolandosorbelli <rolando.sorbelli@gmail.com>
2018-01-09 13:32:55 +0000

    Created a separate file to get data from the API

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,14 +1,15 @@
+require "./lib/getData.rb"
+
 class Controller
 
-	def purchases_data
-    purchases = 'https://driftrock-dev-test-2.herokuapp.com/purchases?per_page=1000&page=1'
-    response = HTTParty.get(purchases)
-    response.parsed_response['data']
-  end
+	attr_reader :getData
 
-  def users_data
-    users = 'https://driftrock-dev-test-2.herokuapp.com/users?per_page=1000&page=1'
-    response = HTTParty.get(users)
-    response.parsed_response['data']
-  end
+	def initialize
+		@getData = GetData.new
+	end
+
+	def run
+		
+	end
+
 end
---
+++ lib/getData.rb
@@ -0,0 +1,16 @@
+require "httparty"
+
+class GetData
+
+	def purchases_data
+    purchases = 'https://driftrock-dev-test-2.herokuapp.com/purchases?per_page=1000&page=1'
+    response = HTTParty.get(purchases)
+    response.parsed_response['data']
+  end
+
+  def users_data
+    users = 'https://driftrock-dev-test-2.herokuapp.com/users?per_page=1000&page=1'
+    response = HTTParty.get(users)
+    response.parsed_response['data']
+  end
+end
